ecb6bf32eb8b48a58eed9c64411b53fc997393b2
KULDEEP MISHRA <158719940+5kul@users.noreply.github.com>
2025-07-27 11:02:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// Function to highlight all <strong> elements
 function highlight() {
-    //Write your code here
-
-
+    // Select all <strong> elements
+    const boldWords = document.querySelectorAll('strong');
+    
+    // Loop through the <strong> elements and change their color to green
+    boldWords.forEach(function(boldWord) {
+        boldWord.style.color = 'rgb(0, 128, 0)';
+    });
 }
 
-
+// Function to return the color of <strong> elements to black
 function return_normal() {
-    //Write your code here
-
+    // Select all <strong> elements
+    const boldWords = document.querySelectorAll('strong');
     
+    // Loop through the <strong> elements and revert their color to black
+    boldWords.forEach(function(boldWord) {
+        boldWord.style.color = 'rgb(0, 0, 0)';
+    });
 }
